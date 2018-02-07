8c54be83ce79db0ef0109b4eb6aea58ab25daafd
jordanlovett <jlovett3@illinois.edu>
2018-02-06 20:45:05 -0600

    called function

--- src/com/company/Webscraper.java
+++ src/com/company/Webscraper.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Webscraper {
     public static void main(String[] args) {
-        String url = "http://erdani.com/tdpl/hamlet.txt";
+        String url = urlToString("https://www.bls.gov/tus/charts/chart9.txt");
+        System.out.println(url);
     }
 
 
@@ -15,17 +16,22 @@ public class Webscraper {
          * @param url url to retrieve contents from
          * @return the contents from the url as a string, or an empty string on error
          */
-        public static String urlToString ( final String url){
-            Scanner urlScanner;
-
-            try {
-                urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
-            } catch (IOException e) {
-                return "";
-            }
-            String contents = urlScanner.useDelimiter("\\A").next();
-            urlScanner.close();
-            return contents;
-            }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
     }
 
